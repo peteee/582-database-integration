3d7fadc575b63e4df94433c82100075334fb0318
Peter Oberlaender <pete@wayoutofline.net>
2023-10-18 13:35:38 -0400

    changing fetch to do asynch/await

--- template/js/view.js
+++ template/js/view.js
@@ -1,20 +1,24 @@
 let viewFormData = document.querySelector("#viewFormData");
 
-function selectData() {
-  fetch("php/select.php")
-    .then((res) => res.json())
-    .then((data) => {
-      for (const item of data) {
-        let singleItem = document.createElement("article");
-        for (const prop in item) {
-          let singleProp = document.createElement("section");
-          singleProp.innerHTML = `${prop} : ${item[prop]}`;
-          singleItem.appendChild(singleProp);
-        }
-        viewFormData.appendChild(singleItem);
+async function selectData() {
+  try {
+    const response = await fetch("php/select.php");
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+    const selection = await response.json();
+    for (const item of selection) {
+      let singleItem = document.createElement("article");
+      for (const prop in item) {
+        let singleProp = document.createElement("section");
+        singleProp.innerHTML = `${prop} : ${item[prop]}`;
+        singleItem.appendChild(singleProp);
       }
-    })
-    .catch((err) => console.log(err));
+      viewFormData.appendChild(singleItem);
+    }
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 }
 
 // run once on page load
